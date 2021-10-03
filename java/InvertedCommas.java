3f1ce812a1f5642a1b154d19a25f0e1ecefeb197
Sarthak Maheshwari <90084829+sarthak1810@users.noreply.github.com>
2021-10-04 00:31:42 +0530

    Add files via upload

---
+++ java/InvertedCommas.java
@@ -0,0 +1,11 @@
+class InvertedCommas {
+
+	public static void main(String[] args) {
+
+		System.out.println();
+		System.out.println("\"Can we print '\\' with System.out.println() statement?\"");
+		System.out.println("010_Sarthak_Maheshwari_IT-3");
+
+	}
+
+}

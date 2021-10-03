691d538e5b5e11506f40471628e0118ede9fd5ee
Jayant <58224270+Jayantk123@users.noreply.github.com>
2021-10-04 00:15:38 +0530

    Add files via upload

---
+++ Competitive Coding/224-basic calculator.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int calculate(String s) {
+          Stack<Integer>st = new Stack<>();
+        int sum=0;
+        int sign=1;
+            
+        
+        
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            
+            if(Character.isDigit(ch))
+            {
+             int val=0;
+             while(i<s.length() && Character.isDigit(s.charAt(i)))
+             {
+                 val=val*10 + (s.charAt(i)-'0');
+                 i++;
+                 
+             }
+             i--;
+             val=val*sign;
+              sign=1;
+             sum+=val;
+            
+                
+            }
+            else if(ch=='(')
+            {
+                
+                st.push(sum);
+                st.push(sign);
+                sum=0;
+                sign=+1;
+                
+            }
+            else if(ch==')')
+            {
+                sum*=st.pop();
+                sum+=st.pop();
+            }
+            else if(ch=='-')
+            {
+                sign*=-1;
+            }
+        }
+        return sum;
+        
+    }
+}

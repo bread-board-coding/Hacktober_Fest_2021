630838e7d9e12f7303f2b48294b4ce420512be36
tg6458 <gargt6458@gmail.com>
2021-10-05 19:45:00 +0530

    Tree

---
+++ Data Structures and Algorithms/Tree/TreeNode.h
@@ -0,0 +1,13 @@
+#include<vector>
+using namespace std;
+template<typename T>
+class TreeNode{
+    public:
+    T data;
+    vector<TreeNode *> children;
+
+    TreeNode(T data)
+    {
+        this->data=data;
+    }
+};

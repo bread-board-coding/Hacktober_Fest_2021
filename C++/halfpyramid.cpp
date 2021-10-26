52117648a017ed331640d412547c4a5cee234d87
swastik2703 <swastikvasistha2127@gmail.com>
2021-10-26 20:59:34 +0530

    half pyramid

---
+++ C++/halfpyramid.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"enter no\n";
+    cin>>n;
+
+    for(int i=n;i>=1;i--)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}

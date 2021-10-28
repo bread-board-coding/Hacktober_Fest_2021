73f12d1e14575929746b56f9f8522b99f8b72cc6
gaurav23122001 <84218006+gaurav23122001@users.noreply.github.com>
2021-10-28 13:35:25 +0530

    Create insertion_sort.cpp

---
+++ C++/Sorting/insertion_sort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 1; i < n; i++)
+    {
+        int temp = a[i];
+        int j = i - 1;
+        while (j >= 0 && a[j] > temp)
+        {
+            a[j + 1] = a[j];
+            j--;
+        }
+        a[j + 1] = temp;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    return 0;
+}

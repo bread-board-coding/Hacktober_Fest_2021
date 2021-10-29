1052b45f22f15fcb93f37e2ea95eafc965947885
BinalSingh <87652737+BinalSingh@users.noreply.github.com>
2021-10-29 19:37:52 +0530

    Create Colored Text with Python.py

---
+++ Python/Colored Text with Python.py
@@ -0,0 +1,8 @@
+
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+"Hi My name is Binal Singh "+ Fore.YELLOW+ Back.BLUE+"I am your Machine Learning Instructor")
+print(Back.CYAN+"Hi My name is Binal Singh")
+print(Fore.RED + Back.GREEN+ "Hi My name is Binal Singh")

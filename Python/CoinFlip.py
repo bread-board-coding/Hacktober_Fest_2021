23c45ae51207fa9d367b914cc6341d206a74df76
lipsha-mishra <61733114+lipsha-mishra@users.noreply.github.com>
2021-10-31 18:12:18 +0530

    Adding some comments...

--- Python/CoinFlip.py
+++ Python/CoinFlip.py
@@ -1,3 +1,5 @@
+# This a Coin Flip program made with Python language
+
 import random
 
 coin = random.randint(1, 2)
@@ -6,3 +8,5 @@ if coin == 1:
   print("You landed on heads!")
 if coin == 2:
   print("You landed on tails!")
+
+# End of the program 

6dcd022a7116b81310534dcd0da8d66c185ac410
mrjayantk237 <93480802+mrjayantk237@users.noreply.github.com>
2021-10-31 20:21:50 +0530

    Add files via upload

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,133 +1,93 @@
-// A complete working C++ program to demonstrate
-// all insertion methods on Linked List
-#include <bits/stdc++.h>
+#include <iostream>
+#include<cstdlib>
+
 using namespace std;
 
-// A linked list node
-class Node
+struct node
 {
-	public:
-	int data;
-	Node *next;
-};
+    int data;
+    struct node *next;
+}*head;
 
-/* Given a reference (pointer to pointer)
-to the head of a list and an int, inserts
-a new node on the front of the list. */
-void push(Node** head_ref, int new_data)
-{
-	/* 1. allocate node */
-	Node* new_node = new Node();
 
-	/* 2. put in the data */
-	new_node->data = new_data;
+void createList(int n)
+    {
+        struct node* temp;
+        int data,i;
 
-	/* 3. Make next of new node as head */
-	new_node->next = (*head_ref);
+       head = (struct node *)malloc(sizeof(struct node));
 
-	/* 4. move the head to point to the new node */
-	(*head_ref) = new_node;
-}
+        if(head == NULL)
+        {
+            cout<<"Unable to locate memory"<<endl;
 
-/* Given a node prev_node, insert a new node after the given
-prev_node */
-void insertAfter(Node* prev_node, int new_data)
-{
-	/*1. check if the given prev_node is NULL */
-	if (prev_node == NULL)
-	{
-		cout<<"the given previous node cannot be NULL";
-		return;
-	}
+        }
+else{
+        cout<<"Enter the data of node 1 :"<<endl;
+        cin >> data;
 
-	/* 2. allocate new node */
-	Node* new_node = new Node();
+        head->data = data;
+        head->next = NULL;
+temp = head;
 
-	/* 3. put in the data */
-	new_node->data = new_data;
+            for(i=2;i<=n;i++)
+            {
+                node* newNode = (struct node *)malloc(sizeof(struct node));
+                if(newNode == NULL)
+                {
+                    cout <<"Unable to locate memory"<<endl;
 
-	/* 4. Make next of new node as next of prev_node */
-	new_node->next = prev_node->next;
+                }
+                else{
+                cout<<"Enter the data of node :"<<i<<endl;
+                cin >> data;
 
-	/* 5. move the next of prev_node as new_node */
-	prev_node->next = new_node;
-}
+                newNode ->data = data;
+                newNode ->next = NULL;
 
-/* Given a reference (pointer to pointer) to the head
-of a list and an int, appends a new node at the end */
-void append(Node** head_ref, int new_data)
-{
-	/* 1. allocate node */
-	Node* new_node = new Node();
+                    temp ->next = newNode;
+                    temp = temp ->next;
+            }
+            }
+    }}
 
-	Node *last = *head_ref; /* used in step 5*/
+    void traverseList( node* n)
+    {
+        struct node* temp;
 
-	/* 2. put in the data */
-	new_node->data = new_data;
 
-	/* 3. This new node is going to be
-	the last node, so make next of
-	it as NULL*/
-	new_node->next = NULL;
+        if(head == NULL)
+        {
+            cout <<"Empty list"<<endl;
 
-	/* 4. If the Linked List is empty,
-	then make the new node as head */
-	if (*head_ref == NULL)
-	{
-		*head_ref = new_node;
-		return;
-	}
+        }
+        else
+        {
 
-	/* 5. Else traverse till the last node */
-	while (last->next != NULL)
-		last = last->next;
 
-	/* 6. Change the next of last node */
-	last->next = new_node;
-	return;
-}
+        temp = head;
+        while(temp != NULL)
+        {
+            cout << temp ->data << " ";
+            temp = temp ->next;
+        }
+    }}
 
-// This function prints contents of
-// linked list starting from head
-void printList(Node *node)
-{
-	while (node != NULL)
-	{
-		cout<<" "<<node->data;
-		node = node->next;
-	}
-}
 
-/* Driver code*/
+
 int main()
 {
-	/* Start with the empty list */
-	Node* head = NULL;
 
-	// Insert 6. So linked list becomes 6->NULL
-	append(&head, 6);
 
-	// Insert 7 at the beginning.
-	// So linked list becomes 7->6->NULL
-	push(&head, 7);
+    int n;
 
-	// Insert 1 at the beginning.
-	// So linked list becomes 1->7->6->NULL
-	push(&head, 1);
+    cout << "Enter total no. of node" << endl;
+    cin >> n;
 
-	// Insert 4 at the end. So
-	// linked list becomes 1->7->6->4->NULL
-	append(&head, 4);
+  createList(n);
 
-	// Insert 8, after 7. So linked
-	// list becomes 1->7->8->6->4->NULL
-	insertAfter(head->next, 8);
+    cout << "Data in list"<<endl;
+    traverseList(head);
 
-	cout<<"Created Linked list is: ";
-	printList(head);
-
-	return 0;
+    return 0;
 }
-
-
-// This code is contributed by rathbhupendra

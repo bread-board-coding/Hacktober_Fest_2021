a8ad2b825e9033867e9a1b873943968d74227fb9
mrjayantk237 <93480802+mrjayantk237@users.noreply.github.com>
2021-10-31 20:16:06 +0530

    Add files via upload

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,44 +1,63 @@
 #include <iostream>
-#include <cstdlibs>
 
 using namespace std;
 
-
-struct node
-{
-int data;
-node* next;
-}
-
-
-void push(node** head_ref, int data)
-{
-node* new_node = (struct node *)malloc(sizeof(struct node));
-
-new_node->data = data;
-new_node->next = *head_ref;
-
-*head_ref = new_data;
-
-
-}
-
-
-void print(node* n)
-{
-while(n != Null)
-{
-cout << n->data;
-n=n->next;
-}
-}
-
 int main()
 {
-    node *head = NULL:
+    int m,n;
+    int arr[20][20];
 
-    push(&head, 2);
+    cout <<"Enter no. of rows and columns" << endl;
+    cin >>m>>n;
 
-    print(head);
+    cout << "Enter value in array" << endl;
+    for(int i=0; i<m; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+
+
+
+      int val = 0;
+    for(int i=0; i<m; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+         if(arr[i][j] != 0)
+            val++;
+        }
+    }
+
+    int spar[3][val];
+    int k=0;
+
+    for(int i=0; i<m; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+           if(arr[i][j] !=0 )
+           {
+               spar[0][k] = i;
+               spar[1][k] = j;
+               spar[2][k] = arr[i][j];
+               k++;
+           }
+        }
+    }
+
+
+cout << "Sparse matrix :" << endl;
+    for(int i=0; i<3; i++)
+    {
+        for(int j=0; j<val; j++)
+        {
+            cout << spar[i][j] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }

451140091f52fdc2b06c24745012f9d38f4aac9a
Ankush Garg <ankushpgarg@gmail.com>
2021-10-31 18:15:13 +0530

    Revert "Adding some comments..."

--- Python/CoinFlip.py
+++ Python/CoinFlip.py
@@ -1,5 +1,3 @@
-# This a Coin Flip program made with Python language
-
 import random
 
 coin = random.randint(1, 2)
@@ -8,5 +6,3 @@ if coin == 1:
   print("You landed on heads!")
 if coin == 2:
   print("You landed on tails!")
-
-# End of the program 
